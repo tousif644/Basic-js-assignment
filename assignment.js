df666548ec7533d7a850a8c991c3f3b80a7e66eb
tousif644 <Tousif2567@gmail.com>
2022-02-05 17:07:49 +0600

    Done

---
+++ assignment.js
@@ -0,0 +1,50 @@
+// Problem - 1
+function anaToVori(ana) {
+  var vori = ana / 16;
+  return vori;
+}
+anaToVori(48);
+
+// Problem - 2
+const singara_price = 7;
+const samosa_price = 10;
+const jilapi_price = 15;
+
+function pandaCost(singara_quantity, samosa_quantity, jilapi_quantity) {
+  var total_singara = singara_price * singara_quantity;
+  var total_samosa = samosa_price * samosa_quantity;
+  var total_jilapi = jilapi_price * jilapi_quantity;
+
+  let total_price = total_singara + total_samosa + total_jilapi;
+
+  return total_price;
+}
+
+pandaCost(2, 0, 0);
+
+// Problem - 3
+function picnicBudget(totalPerson) {
+  if (totalPerson <= 100) {
+    return 5000;
+  } else if (totalPerson >= 101 && totalPerson <= 200) {
+    return 4000;
+  } else if (totalPerson >= 201) {
+    return 3000;
+  }
+}
+
+picnicBudget(250);
+
+// Problem - 4
+function oddFriend(names) {
+  let odd_friend = [];
+  for (let i = 0; i < names.length; i++) {
+    let temp = names[i].length;
+    if (temp % 2 == 0) {
+      odd_friend.push(names[i]);
+    }
+  }
+  return odd_friend;
+}
+
+oddFriend(["Jerry", "Thomas", "Smith", "Evan"]);
